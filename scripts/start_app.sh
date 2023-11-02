8cb20ad875eb34a70c436354cd70f049ed9dbe59
Surendra <66915927+Surendra-Chakali@users.noreply.github.com>
2023-11-02 18:19:23 +0530

    updated image tag

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,4 +3,4 @@
 echo "Starting Nginx server on EC2"
 
 
-docker run --name suriDemoNginx -dp 80:80 762714347827.dkr.ecr.ap-south-1.amazonaws.com/demoproject:1
+docker run --name suriDemoNginx -dp 80:80 762714347827.dkr.ecr.ap-south-1.amazonaws.com/demoproject:37
