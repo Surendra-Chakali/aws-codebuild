b985cfbae132f01b5ce2ac4881f72e6c5dd112e8
Surendra <66915927+Surendra-Chakali@users.noreply.github.com>
2023-11-02 18:17:01 +0530

    Modified files

--- scripts/pull_app.sh
+++ scripts/pull_app.sh
@@ -2,6 +2,6 @@
 
 echo "Pulling image from ECR"
 
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 762714347827.dkr.ecr.ap-south-1.amazonaws.com/surendra-ci-artifact
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 762714347827.dkr.ecr.ap-south-1.amazonaws.com/demoproject
 
-docker pull 762714347827.dkr.ecr.ap-south-1.amazonaws.com/surendra-ci-artifact:35
+docker pull 762714347827.dkr.ecr.ap-south-1.amazonaws.com/demoproject:1
