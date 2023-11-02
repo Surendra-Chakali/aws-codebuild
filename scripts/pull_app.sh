bd3e2e5cca1187709b22e30572d7403b370e769c
Surendra <66915927+Surendra-Chakali@users.noreply.github.com>
2023-11-02 14:54:56 +0530

    added appsec yaml file

---
+++ scripts/pull_app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Pulling image from ECR"
+
+docker pull 762714347827.dkr.ecr.ap-south-1.amazonaws.com/surendra-ci-artifact:latest
---
+++ scripts/start_app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Starting Nginx server on EC2"
+
+
+docker run --name suriDemoNginx -dp 80:80 762714347827.dkr.ecr.ap-south-1.amazonaws.com/surendra-ci-artifact:latest
